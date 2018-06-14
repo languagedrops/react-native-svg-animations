cec786de963cbbfb57379dc53d0e5edd00d55fd3
Samu András <samu.andris1@gmail.com>
2018-06-14 12:25:14 +0200

    fix: set animationLimit to toValue instead of 0 const

--- components/AnimatedSVGPath/index.js
+++ components/AnimatedSVGPath/index.js
@@ -93,7 +93,7 @@ class AnimatedSVGPath extends Component {
     Animated.sequence([
       Animated.delay(delay),
       Animated.timing(this.state.strokeDashOffset, {
-        toValue: 0,
+        toValue: animationLimit,
         duration: animationDuration,
       })
     ]).start(() => {
